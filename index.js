0a5d8f55ee197af7bb9cf569d6f6fde09dc865fa
Katrina R <katrina.reinsdorff@gmail.com>
2019-06-26 21:55:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b){
+  return(a+b)
+}
+
+add(2, 3)
+
+function subtract(a, b) {
+  return(a-b)
+}
+
+subtract(2, 3)
+
+function multiply(a, b) {
+  return(a*b)
+}
+
+multiply(2, 3)
+
+function divide(a, b) {
+  return(a/b)
+}
+
+divide(6, 2)
+
+function inc(n) {
+  return(n + 1)
+}
+
+inc(3)
+
+function dec(n) {
+  return(n - 1)
+}
+dec(5)
+
+function makeInt(n, p) {
+  return(parseInt(n, p))
+}
+
+makeInt(2, 10)
+
+function makeInt(n, p) {
+  return(parseInt(n * p))
+}
+
+makeInt(0x2328, 10)
+
