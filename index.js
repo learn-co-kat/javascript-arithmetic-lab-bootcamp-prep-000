5e8791fa77722d87e4d29c9903e3fbc73c3e0ace
Katrina R <katrina.reinsdorff@gmail.com>
2019-06-26 22:27:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,3 +54,13 @@ function makeInt(n) {
 makeInt(2, 10)
 makeInt((0*2328), 10)
 makeInt(sldkjflksjf, 10)
+
+// describe('preserveDecimal(n)', function() {
+//   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+//     expect(preserveDecimal('2.222')).toBe(2.222)
+//   })
+
+//   it('returns NaN as appropriate', function() {
+//     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+//   })
+// })
